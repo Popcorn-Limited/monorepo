c848db576072292b8845a39260ffbfd7e40e09b5
RedVeil <44397805+RedVeil@users.noreply.github.com>
2023-02-02 07:51:46 +0100

    Update ConvexAdapter.sol

--- packages/foundry/src/vault/adapter/convex/ConvexAdapter.sol
+++ packages/foundry/src/vault/adapter/convex/ConvexAdapter.sol
@@ -1,127 +1,172 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.10;
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.10;
 
-import { MidasERC4626 } from "./MidasERC4626.sol";
-import { FixedPointMathLib } from "solmate/utils/FixedPointMathLib.sol";
-import { FlywheelCore } from "flywheel-v2/FlywheelCore.sol";
-import { RewardsClaimer } from "../RewardsClaimer.sol";
+import {ERC20, ERC4626} from "solmate/mixins/ERC4626.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
-import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+/// @title Rewards Claiming Contract
+/// @author joeysantoro
+contract RewardsClaimer {
+    using SafeTransferLib for ERC20;
 
-interface ILpDepositor {
-  // user -> pool -> deposit amount
-  function userBalances(address _user, ERC20Upgradeable _token) external view returns (uint256);
+    event RewardDestinationUpdate(address indexed newDestination);
 
-  function deposit(
-    address _user,
-    ERC20Upgradeable _token,
-    uint256 _amount
-  ) external;
+    event ClaimRewards(address indexed rewardToken, uint256 amount);
 
-  function withdraw(
-    address _receiver,
-    ERC20Upgradeable _token,
-    uint256 _amount
-  ) external;
+    /// @notice the address to send rewards
+    address public rewardDestination;
 
-  function claim(
-    address _receiver,
-    ERC20Upgradeable[] calldata _tokens,
-    uint256 _maxBondAmount
-  ) external;
+    /// @notice the array of reward tokens to send to
+    ERC20[] public rewardTokens;
 
-  function depositTokens(ERC20Upgradeable lpToken) external view returns (ERC20Upgradeable);
+    constructor(address _rewardDestination, ERC20[] memory _rewardTokens) {
+        rewardDestination = _rewardDestination;
+        rewardTokens = _rewardTokens;
+    }
+
+    /// @notice claim all token rewards
+    function claimRewards() public {
+        beforeClaim(); // hook to accrue/pull in rewards, if needed
+
+        uint256 len = rewardTokens.length;
+        // send all tokens to destination
+        for (uint256 i = 0; i < len; i++) {
+            ERC20 token = rewardTokens[i];
+            uint256 amount = token.balanceOf(address(this));
+
+            token.safeTransfer(rewardDestination, amount);
+
+            emit ClaimRewards(address(token), amount);
+        }
+    }
+
+    /// @notice set the address of the new reward destination
+    /// @param newDestination the new reward destination
+    function setRewardDestination(address newDestination) external {
+        require(msg.sender == rewardDestination, "UNAUTHORIZED");
+        rewardDestination = newDestination;
+        emit RewardDestinationUpdate(newDestination);
+    }
+
+    /// @notice hook to accrue/pull in rewards, if needed
+    function beforeClaim() internal virtual {}
 }
 
-/**
- * @title DotDot LpToken ERC4626 Contract
- * @notice ERC4626 Strategy using DotDotFinance for Ellipsis LP-Token
- * @author RedVeil
- *
- * Stakes and withdraws deposited LP-Token in/from https://github.com/dotdot-ellipsis/dotdot-contracts/blob/main/contracts/LpDepositor.sol
- * and claims rewards from the same contract
- *
- */
-contract DotDotLpERC4626 is MidasERC4626, RewardsClaimer {
-  using FixedPointMathLib for uint256;
+// Docs: https://docs.convexfinance.com/convexfinanceintegration/booster
 
-  /* ========== STATE VARIABLES ========== */
-  FlywheelCore public dddFlywheel;
-  FlywheelCore public epxFlywheel;
-  ILpDepositor public lpDepositor;
-  ERC20Upgradeable[] public assetAsArray;
+// main Convex contract(booster.sol) basic interface
+interface IConvexBooster {
+    // deposit into convex, receive a tokenized deposit. parameter to stake immediately
+    function deposit(
+        uint256 _pid,
+        uint256 _amount,
+        bool _stake
+    ) external returns (bool);
+}
 
-  /* ========== INITIALIZER ========== */
+interface IConvexBaseRewardPool {
+    function pid() external view returns (uint256);
 
-  /**
-     @notice Initializes the Vault.
-     @param asset The ERC20 compliant token the Vault should accept.
-     @param _dddFlywheel Flywheel to pull DDD rewards
-     @param _epxFlywheel Flywheel to pull EPX rewards
-     @param _lpDepositor DotDot deposit contract for LpToken
+    function withdrawAndUnwrap(uint256 amount, bool claim)
+        external
+        returns (bool);
+
+    function getReward(address _account, bool _claimExtras)
+        external
+        returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function extraRewards(uint256 index) external view returns (IRewards);
+
+    function extraRewardsLength() external view returns (uint256);
+
+    function rewardToken() external view returns (ERC20);
+}
+
+interface IRewards {
+    function rewardToken() external view returns (ERC20);
+}
+
+/// @title Convex Finance Yield Bearing Vault
+/// @author joeysantoro
+contract ConvexERC4626 is ERC4626, RewardsClaimer {
+    using SafeTransferLib for ERC20;
+
+    /// @notice The Convex Booster contract (for deposit/withdraw)
+    IConvexBooster public immutable convexBooster;
+
+    /// @notice The Convex Rewards contract (for claiming rewards)
+    IConvexBaseRewardPool public immutable convexRewards;
+
+    /// @notice Convex token
+    ERC20 CVX = ERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
+
+    uint256 public immutable pid;
+
+    /**
+     @notice Creates a new Vault that accepts a specific underlying token.
+     @param _asset The ERC20 compliant token the Vault should accept.
+     @param _name The name for the vault token.
+     @param _symbol The symbol for the vault token.
+     @param _convexBooster The Convex Booster contract (for deposit/withdraw).
+     @param _convexRewards The Convex Rewards contract (for claiming rewards).
+     @param _rewardsDestination the address to send CRV and CVX.
+     @param _rewardTokens the rewards tokens to send out.
     */
-  function initialize(
-    ERC20Upgradeable asset,
-    FlywheelCore _dddFlywheel,
-    FlywheelCore _epxFlywheel,
-    ILpDepositor _lpDepositor,
-    address _rewardsDestination,
-    ERC20Upgradeable[] memory _rewardTokens
-  ) public initializer {
-    __MidasER4626_init(asset);
-    __RewardsClaimer_init(_rewardsDestination, _rewardTokens);
+    constructor(
+        ERC20 _asset,
+        string memory _name,
+        string memory _symbol,
+        IConvexBooster _convexBooster,
+        IConvexBaseRewardPool _convexRewards,
+        address _rewardsDestination,
+        ERC20[] memory _rewardTokens
+    )
+        ERC4626(_asset, _name, _symbol)
+        RewardsClaimer(_rewardsDestination, _rewardTokens)
+    {
+        convexBooster = _convexBooster;
+        convexRewards = _convexRewards;
 
-    performanceFee = 5e16;
-    dddFlywheel = _dddFlywheel;
-    epxFlywheel = _epxFlywheel;
-    lpDepositor = _lpDepositor;
+        pid = _convexRewards.pid();
 
-    // lpDepositor wants an address array for claiming
-    assetAsArray.push(asset);
+        _asset.approve(address(_convexBooster), type(uint256).max);
+    }
 
-    asset.approve(address(lpDepositor), type(uint256).max);
-  }
+    function updateRewardTokens() public {
+        uint256 len = convexRewards.extraRewardsLength();
+        require(len < 5, "exceed max rewards");
+        delete rewardTokens;
+        bool cvxReward;
 
-  /* ========== VIEWS ========== */
+        for (uint256 i = 0; i < len; i++) {
+            rewardTokens.push(convexRewards.extraRewards(i).rewardToken());
+            if (convexRewards.extraRewards(i).rewardToken() == CVX)
+                cvxReward = true;
+        }
+        if (!cvxReward) rewardTokens.push(CVX);
+        rewardTokens.push(convexRewards.rewardToken());
+    }
 
-  /// @notice Calculates the total amount of underlying tokens the Vault holds.
-  /// @return The total amount of underlying tokens the Vault holds.
-  function totalAssets() public view override returns (uint256) {
-    return paused() ? _asset().balanceOf(address(this)) : lpDepositor.userBalances(address(this), _asset());
-  }
+    function afterDeposit(uint256 amount, uint256) internal override {
+        require(convexBooster.deposit(pid, amount, true), "deposit error");
+    }
 
-  /// @notice Calculates the total amount of underlying tokens the user holds.
-  /// @return The total amount of underlying tokens the user holds.
-  function balanceOfUnderlying(address account) public view returns (uint256) {
-    return convertToAssets(balanceOf(account));
-  }
+    function beforeWithdraw(uint256 amount, uint256) internal override {
+        require(
+            convexRewards.withdrawAndUnwrap(amount, false),
+            "withdraw error"
+        );
+    }
 
-  /* ========== INTERNAL FUNCTIONS ========== */
+    function beforeClaim() internal override {
+        require(convexRewards.getReward(address(this), true), "rewards error");
+    }
 
-  function afterDeposit(uint256 amount, uint256) internal override {
-    lpDepositor.deposit(address(this), _asset(), amount);
-    lpDepositor.claim(address(this), assetAsArray, 0);
-  }
-
-  /// @notice withdraws specified amount of underlying token if possible
-  function beforeWithdraw(uint256 amount, uint256) internal override {
-    lpDepositor.withdraw(address(this), _asset(), amount);
-    lpDepositor.claim(address(this), assetAsArray, 0);
-  }
-
-  function beforeClaim() internal override {
-    lpDepositor.claim(address(this), assetAsArray, 0);
-  }
-
-  /* ========== EMERGENCY FUNCTIONS ========== */
-
-  function emergencyWithdrawAndPause() external override onlyOwner {
-    lpDepositor.withdraw(address(this), _asset(), lpDepositor.depositTokens(_asset()).balanceOf(address(this)));
-    _pause();
-  }
-
-  function unpause() external override onlyOwner {
-    _unpause();
-    lpDepositor.deposit(address(this), _asset(), _asset().balanceOf(address(this)));
-  }
+    /// @notice Calculates the total amount of underlying tokens the Vault holds.
+    /// @return The total amount of underlying tokens the Vault holds.
+    function totalAssets() public view override returns (uint256) {
+        return convexRewards.balanceOf(address(this));
+    }
 }
