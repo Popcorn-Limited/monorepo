7b05d2a596594880dbff4c8440670a1b91085f36
amatureApe <amatureApe@gmail.com>
2023-02-06 11:58:14 -0800

    convexAdapter first draft

--- packages/foundry/src/vault/adapter/convex/ConvexAdapter.sol
+++ packages/foundry/src/vault/adapter/convex/ConvexAdapter.sol
@@ -1,172 +1,121 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.10;
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
 
-import {ERC20, ERC4626} from "solmate/mixins/ERC4626.sol";
-import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+pragma solidity ^0.8.15;
 
-/// @title Rewards Claiming Contract
-/// @author joeysantoro
-contract RewardsClaimer {
-    using SafeTransferLib for ERC20;
+import { AdapterBase, IERC20, IERC20Metadata, SafeERC20, ERC20, Math, IStrategy, IAdapter } from "../abstracts/AdapterBase.sol";
+import { WithRewards, IWithRewards } from "../abstracts/WithRewards.sol";
+import { IConvexBooster, IBaseRewarder } from "./IConvex.sol";
 
-    event RewardDestinationUpdate(address indexed newDestination);
+/**
+ * @title   Convex Adapter
+ * @author  amatureApe
+ * @notice  ERC4626 wrapper for Convex Vaults.
+ *
+ * An ERC4626 compliant Wrapper for https://github.com/beefyfinance/beefy-contracts/blob/master/contracts/BIFI/vaults/BeefyVaultV6.sol.
+ * Allows wrapping Convex Vaults with or without an active Booster.
+ * Allows for additional strategies to use rewardsToken in case of an active Booster.
+ */
+contract ConvexAdapter is AdapterBase, WithRewards {
+  using SafeERC20 for IERC20;
+  using Math for uint256;
 
-    event ClaimRewards(address indexed rewardToken, uint256 amount);
+  string internal _name;
+  string internal _symbol;
 
-    /// @notice the address to send rewards
-    address public rewardDestination;
+  /// @notice The poolId inside Convex booster for relevant Curve lpToken.
+  uint256 public pid;
 
-    /// @notice the array of reward tokens to send to
-    ERC20[] public rewardTokens;
+  /// @notice The booster address for Convex
+  IConvexBooster public booster;
 
-    constructor(address _rewardDestination, ERC20[] memory _rewardTokens) {
-        rewardDestination = _rewardDestination;
-        rewardTokens = _rewardTokens;
-    }
+  /// @notice The Convex BaseRewarder.
+  IBaseRewarder public baseRewarder;
 
-    /// @notice claim all token rewards
-    function claimRewards() public {
-        beforeClaim(); // hook to accrue/pull in rewards, if needed
+  /*//////////////////////////////////////////////////////////////
+                            INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
 
-        uint256 len = rewardTokens.length;
-        // send all tokens to destination
-        for (uint256 i = 0; i < len; i++) {
-            ERC20 token = rewardTokens[i];
-            uint256 amount = token.balanceOf(address(this));
+  /**
+   * @notice Initialize a new Convex Adapter.
+   * @param adapterInitData Encoded data for the base adapter initialization.
+   * @param convexInitData Encoded data for the convex adapter initialization.
+   * @dev `_booster` - The booster address for Convex.
+   * @dev `_pid` - The poolId for lpToken.
+   * @dev This function is called by the factory contract when deploying a new vault.
+   */
+  function initialize(
+    bytes memory adapterInitData,
+    address,
+    bytes memory convexInitData
+  ) public {
+    (address _booster, uint256 _pid) = abi.decode(convexInitData, (address, uint256));
+    __AdapterBase_init(adapterInitData);
 
-            token.safeTransfer(rewardDestination, amount);
+    booster = IConvexBooster(_booster);
+    pid = _pid;
 
-            emit ClaimRewards(address(token), amount);
-        }
-    }
+    (, , , address _baseRewarder, , ) = booster.poolInfo(pid);
+    baseRewarder = IBaseRewarder(_baseRewarder);
 
-    /// @notice set the address of the new reward destination
-    /// @param newDestination the new reward destination
-    function setRewardDestination(address newDestination) external {
-        require(msg.sender == rewardDestination, "UNAUTHORIZED");
-        rewardDestination = newDestination;
-        emit RewardDestinationUpdate(newDestination);
-    }
+    _name = string.concat("Popcorn Convex", IERC20Metadata(asset()).name(), " Adapter");
+    _symbol = string.concat("popB-", IERC20Metadata(asset()).symbol());
 
-    /// @notice hook to accrue/pull in rewards, if needed
-    function beforeClaim() internal virtual {}
-}
+    IERC20(asset()).approve(address(booster), type(uint256).max);
+  }
 
-// Docs: https://docs.convexfinance.com/convexfinanceintegration/booster
+  function name() public view override(IERC20Metadata, ERC20) returns (string memory) {
+    return _name;
+  }
 
-// main Convex contract(booster.sol) basic interface
-interface IConvexBooster {
-    // deposit into convex, receive a tokenized deposit. parameter to stake immediately
-    function deposit(
-        uint256 _pid,
-        uint256 _amount,
-        bool _stake
-    ) external returns (bool);
-}
+  function symbol() public view override(IERC20Metadata, ERC20) returns (string memory) {
+    return _symbol;
+  }
 
-interface IConvexBaseRewardPool {
-    function pid() external view returns (uint256);
+  /*//////////////////////////////////////////////////////////////
+                            ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
 
-    function withdrawAndUnwrap(uint256 amount, bool claim)
-        external
-        returns (bool);
+  /// @notice Calculates the total amount of underlying tokens the Vault holds.
+  /// @return The total amount of underlying tokens the Vault holds.
+  function totalAssets() public view override returns (uint256) {
+    return paused() ? IERC20(asset()).balanceOf(address(this)) : baseRewarder.balanceOf(address(this));
+  }
 
-    function getReward(address _account, bool _claimExtras)
-        external
-        returns (bool);
+  /// @notice Calculates the total amount of underlying tokens the user holds.
+  /// @return The total amount of underlying tokens the user holds.
+  function balanceOfUnderlying(address account) public view returns (uint256) {
+    return convertToAssets(balanceOf(account));
+  }
 
-    function balanceOf(address account) external view returns (uint256);
+  function previewWithdraw(uint256 assets) public view override returns (uint256) {
+    return _convertToShares(assets, Math.Rounding.Up);
+  }
 
-    function extraRewards(uint256 index) external view returns (IRewards);
+  function previewRedeem(uint256 shares) public view override returns (uint256) {
+    return _convertToAssets(shares, Math.Rounding.Down);
+  }
 
-    function extraRewardsLength() external view returns (uint256);
+  /*//////////////////////////////////////////////////////////////
+                          INTERNAL HOOKS LOGIC
+    //////////////////////////////////////////////////////////////*/
 
-    function rewardToken() external view returns (ERC20);
-}
+  /// @notice Deposit into Convex booster contract
+  function _protocolDeposit(uint256 amount, uint256) internal virtual override {
+    booster.deposit(pid, amount, true);
+  }
 
-interface IRewards {
-    function rewardToken() external view returns (ERC20);
-}
+  /// @notice Withdraw from Convex booster contract
+  function _protocolWithdraw(uint256, uint256 shares) internal virtual override {
+    uint256 convexShares = convertToUnderlyingShares(0, shares);
+    booster.withdraw(pid, convexShares);
+  }
 
-/// @title Convex Finance Yield Bearing Vault
-/// @author joeysantoro
-contract ConvexERC4626 is ERC4626, RewardsClaimer {
-    using SafeTransferLib for ERC20;
+  /*//////////////////////////////////////////////////////////////
+                      EIP-165 LOGIC
+  //////////////////////////////////////////////////////////////*/
 
-    /// @notice The Convex Booster contract (for deposit/withdraw)
-    IConvexBooster public immutable convexBooster;
-
-    /// @notice The Convex Rewards contract (for claiming rewards)
-    IConvexBaseRewardPool public immutable convexRewards;
-
-    /// @notice Convex token
-    ERC20 CVX = ERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
-
-    uint256 public immutable pid;
-
-    /**
-     @notice Creates a new Vault that accepts a specific underlying token.
-     @param _asset The ERC20 compliant token the Vault should accept.
-     @param _name The name for the vault token.
-     @param _symbol The symbol for the vault token.
-     @param _convexBooster The Convex Booster contract (for deposit/withdraw).
-     @param _convexRewards The Convex Rewards contract (for claiming rewards).
-     @param _rewardsDestination the address to send CRV and CVX.
-     @param _rewardTokens the rewards tokens to send out.
-    */
-    constructor(
-        ERC20 _asset,
-        string memory _name,
-        string memory _symbol,
-        IConvexBooster _convexBooster,
-        IConvexBaseRewardPool _convexRewards,
-        address _rewardsDestination,
-        ERC20[] memory _rewardTokens
-    )
-        ERC4626(_asset, _name, _symbol)
-        RewardsClaimer(_rewardsDestination, _rewardTokens)
-    {
-        convexBooster = _convexBooster;
-        convexRewards = _convexRewards;
-
-        pid = _convexRewards.pid();
-
-        _asset.approve(address(_convexBooster), type(uint256).max);
-    }
-
-    function updateRewardTokens() public {
-        uint256 len = convexRewards.extraRewardsLength();
-        require(len < 5, "exceed max rewards");
-        delete rewardTokens;
-        bool cvxReward;
-
-        for (uint256 i = 0; i < len; i++) {
-            rewardTokens.push(convexRewards.extraRewards(i).rewardToken());
-            if (convexRewards.extraRewards(i).rewardToken() == CVX)
-                cvxReward = true;
-        }
-        if (!cvxReward) rewardTokens.push(CVX);
-        rewardTokens.push(convexRewards.rewardToken());
-    }
-
-    function afterDeposit(uint256 amount, uint256) internal override {
-        require(convexBooster.deposit(pid, amount, true), "deposit error");
-    }
-
-    function beforeWithdraw(uint256 amount, uint256) internal override {
-        require(
-            convexRewards.withdrawAndUnwrap(amount, false),
-            "withdraw error"
-        );
-    }
-
-    function beforeClaim() internal override {
-        require(convexRewards.getReward(address(this), true), "rewards error");
-    }
-
-    /// @notice Calculates the total amount of underlying tokens the Vault holds.
-    /// @return The total amount of underlying tokens the Vault holds.
-    function totalAssets() public view override returns (uint256) {
-        return convexRewards.balanceOf(address(this));
-    }
+  function supportsInterface(bytes4 interfaceId) public pure override(WithRewards, AdapterBase) returns (bool) {
+    return interfaceId == type(IWithRewards).interfaceId || interfaceId == type(IAdapter).interfaceId;
+  }
 }
---
+++ packages/foundry/src/vault/adapter/convex/IConvex.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.10;
+
+interface IConvexBooster {
+  function deposit(
+    uint256 pid,
+    uint256 amount,
+    bool stake
+  ) external;
+
+  function withdraw(uint256 pid, uint256 amount) external;
+
+  function poolInfo(uint256 pid)
+    external
+    view
+    returns (
+      address lpToken,
+      address token,
+      address gauge,
+      address crvRewards,
+      address stash,
+      bool shutdown
+    );
+}
+
+interface IBaseRewarder {
+  function balanceOf(address addr) external view returns (uint256);
+}
