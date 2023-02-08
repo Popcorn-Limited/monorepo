089703442d5a2039557aa4b7cc4fe2043b3c82b4
RedVeil <leon.niesler@gmail.com>
2023-02-08 15:53:22 +0100

    overriding compound adapter test -wip

--- packages/foundry/test/vault/integration/compoundV2/CompoundV2Adapter.t.sol
+++ packages/foundry/test/vault/integration/compoundV2/CompoundV2Adapter.t.sol
@@ -15,6 +15,8 @@ contract CompoundV2AdapterTest is AbstractAdapterTest {
   ICToken cToken;
   IComptroller comptroller;
 
+  uint256 compoundDefaultAmount = 1e9;
+
   function setUp() public {
     uint256 forkId = vm.createSelectFork(vm.rpcUrl("mainnet"));
     vm.selectFork(forkId);
@@ -69,10 +71,10 @@ contract CompoundV2AdapterTest is AbstractAdapterTest {
   // Verify that totalAssets returns the expected amount
   function verify_totalAssets() public override {
     // Make sure totalAssets isn't 0
-    deal(address(asset), bob, defaultAmount);
+    deal(address(asset), bob, compoundDefaultAmount);
     vm.startPrank(bob);
-    asset.approve(address(adapter), defaultAmount);
-    adapter.deposit(defaultAmount, bob);
+    asset.approve(address(adapter), compoundDefaultAmount);
+    adapter.deposit(compoundDefaultAmount, bob);
     vm.stopPrank();
 
     assertEq(
@@ -107,27 +109,263 @@ contract CompoundV2AdapterTest is AbstractAdapterTest {
   }
 
   /*//////////////////////////////////////////////////////////////
-                              HARVEST
+                          PREVIEW VIEWS
     //////////////////////////////////////////////////////////////*/
 
-  function test__cTokenDeposit() public {
-    _mintFor(defaultAmount, address(this));
-    asset.approve(address(cToken), defaultAmount);
-    cToken.mint(1e9);
-    emit log_uint(cToken.balanceOf(address(this)));
+  function test__previewDeposit(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxAssets);
+
+    deal(address(asset), bob, maxAssets);
+    vm.prank(bob);
+    asset.approve(address(adapter), maxAssets);
+
+    prop_previewDeposit(bob, bob, amount, testId);
   }
 
-  function test__harvest() public override {
-    _mintFor(defaultAmount, bob);
+  function test__previewMint(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxShares);
+
+    deal(address(asset), bob, maxAssets);
+    vm.prank(bob);
+    asset.approve(address(adapter), maxAssets);
+
+    prop_previewMint(bob, bob, amount, testId);
+  }
+
+  function test__previewWithdraw(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxAssets);
+
+    uint256 reqAssets = (adapter.previewMint(adapter.previewWithdraw(amount)) * 10) / 9;
+    _mintFor(reqAssets, bob);
+    vm.prank(bob);
+    adapter.deposit(reqAssets, bob);
+
+    prop_previewWithdraw(bob, bob, bob, amount, testId);
+  }
+
+  function test__previewRedeem(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxShares);
+
+    uint256 reqAssets = (adapter.previewMint(amount) * 10) / 9;
+    _mintFor(reqAssets, bob);
+    vm.prank(bob);
+    adapter.deposit(reqAssets, bob);
+
+    prop_previewRedeem(bob, bob, bob, amount, testId);
+  }
+
+    /*//////////////////////////////////////////////////////////////
+                    DEPOSIT/MINT/WITHDRAW/REDEEM
+    //////////////////////////////////////////////////////////////*/
+
+  function test__deposit(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxAssets);
+    uint8 len = uint8(testConfigStorage.getTestConfigLength());
+    for (uint8 i; i < len; i++) {
+      if (i > 0) overrideSetup(testConfigStorage.getTestConfig(i));
+
+      _mintFor(amount, bob);
+      prop_deposit(bob, bob, amount, testId);
+
+      increasePricePerShare(raise);
+
+      _mintFor(amount, bob);
+      prop_deposit(bob, alice, amount, testId);
+    }
+  }
+
+  function test__mint(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxShares);
+    uint8 len = uint8(testConfigStorage.getTestConfigLength());
+    for (uint8 i; i < len; i++) {
+      if (i > 0) overrideSetup(testConfigStorage.getTestConfig(i));
+
+      _mintFor(adapter.previewMint(amount), bob);
+      prop_mint(bob, bob, amount, testId);
+
+      increasePricePerShare(raise);
+
+      _mintFor(adapter.previewMint(amount), bob);
+      prop_mint(bob, alice, amount, testId);
+    }
+  }
+
+  function test__withdraw(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxAssets);
+    uint8 len = uint8(testConfigStorage.getTestConfigLength());
+    for (uint8 i; i < len; i++) {
+      if (i > 0) overrideSetup(testConfigStorage.getTestConfig(i));
+
+      uint256 reqAssets = (adapter.previewMint(adapter.previewWithdraw(amount)) * 10) / 8;
+      _mintFor(reqAssets, bob);
+      vm.prank(bob);
+      adapter.deposit(reqAssets, bob);
+      emit log_named_uint("ts",adapter.totalSupply());
+      emit log_named_uint("ta",adapter.totalAssets());
+      prop_withdraw(bob, bob, amount, testId);
+
+      _mintFor(reqAssets, bob);
+      vm.prank(bob);
+      adapter.deposit(reqAssets, bob);
+
+      increasePricePerShare(raise);
+
+      vm.prank(bob);
+      adapter.approve(alice, type(uint256).max);
+      prop_withdraw(alice, bob, amount, testId);
+    }
+  }
+
+  function test__redeem(uint8 fuzzAmount) public override {
+    uint256 amount = bound(uint256(fuzzAmount), 1e9, maxShares);
+    uint8 len = uint8(testConfigStorage.getTestConfigLength());
+    for (uint8 i; i < len; i++) {
+      if (i > 0) overrideSetup(testConfigStorage.getTestConfig(i));
+
+      uint256 reqAssets = (adapter.previewMint(amount) * 10) / 9;
+      _mintFor(reqAssets, bob);
+      vm.prank(bob);
+      adapter.deposit(reqAssets, bob);
+      prop_redeem(bob, bob, amount, testId);
+
+      _mintFor(reqAssets, bob);
+      vm.prank(bob);
+      adapter.deposit(reqAssets, bob);
+
+      increasePricePerShare(raise);
+
+      vm.prank(bob);
+      adapter.approve(alice, type(uint256).max);
+      prop_redeem(alice, bob, amount, testId);
+    }
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                          ROUNDTRIP TESTS
+    //////////////////////////////////////////////////////////////*/
+
+  function test__RT_deposit_redeem() public override {
+    _mintFor(compoundDefaultAmount, bob);
+
+    vm.startPrank(bob);
+    uint256 shares = adapter.deposit(compoundDefaultAmount, bob);
+    uint256 assets = adapter.redeem(shares, bob, bob);
+    vm.stopPrank();
+
+    assertLe(assets, compoundDefaultAmount, testId);
+  }
+
+  function test__RT_deposit_withdraw() public override {
+    _mintFor(compoundDefaultAmount, bob);
+
+    vm.startPrank(bob);
+    uint256 shares1 = adapter.deposit(compoundDefaultAmount, bob);
+    uint256 shares2 = adapter.withdraw(compoundDefaultAmount, bob, bob);
+    vm.stopPrank();
+
+    assertGe(shares2, shares1, testId);
+  }
+
+  function test__RT_mint_withdraw() public override {
+    _mintFor(adapter.previewMint(compoundDefaultAmount), bob);
+
+    vm.startPrank(bob);
+    uint256 assets = adapter.mint(compoundDefaultAmount, bob);
+    uint256 shares = adapter.withdraw(assets, bob, bob);
+    vm.stopPrank();
+
+    assertGe(shares, compoundDefaultAmount, testId);
+  }
+
+  function test__RT_mint_redeem() public override {
+    _mintFor(adapter.previewMint(compoundDefaultAmount), bob);
+
+    vm.startPrank(bob);
+    uint256 assets1 = adapter.mint(compoundDefaultAmount, bob);
+    uint256 assets2 = adapter.redeem(compoundDefaultAmount, bob, bob);
+    vm.stopPrank();
+
+    assertLe(assets2, assets1, testId);
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                              PAUSE
+    //////////////////////////////////////////////////////////////*/
+
+  function test__pause() public override {
+    _mintFor(compoundDefaultAmount, bob);
 
     vm.prank(bob);
-    adapter.deposit(defaultAmount, bob);
+    adapter.deposit(compoundDefaultAmount, bob);
+
+    uint256 oldTotalAssets = adapter.totalAssets();
+    uint256 oldTotalSupply = adapter.totalSupply();
+
+    adapter.pause();
+
+    // We simply withdraw into the adapter
+    // TotalSupply and Assets dont change
+    assertApproxEqAbs(oldTotalAssets, adapter.totalAssets(), _delta_, "totalAssets");
+    assertApproxEqAbs(oldTotalSupply, adapter.totalSupply(), _delta_, "totalSupply");
+    assertApproxEqAbs(asset.balanceOf(address(adapter)), oldTotalAssets, _delta_, "asset balance");
+    assertApproxEqAbs(iouBalance(), 0, _delta_, "iou balance");
+
+    vm.startPrank(bob);
+    // Deposit and mint are paused (maxDeposit/maxMint are set to 0 on pause)
+    vm.expectRevert(abi.encodeWithSelector(MaxError.selector, compoundDefaultAmount));
+    adapter.deposit(compoundDefaultAmount, bob);
+
+    vm.expectRevert(abi.encodeWithSelector(MaxError.selector, compoundDefaultAmount));
+    adapter.mint(compoundDefaultAmount, bob);
+
+    // Withdraw and Redeem dont revert
+    adapter.withdraw(compoundDefaultAmount / 10, bob, bob);
+    adapter.redeem(compoundDefaultAmount / 10, bob, bob);
+  }
+
+
+  function test__unpause() public override {
+    _mintFor(compoundDefaultAmount * 3, bob);
+
+    vm.prank(bob);
+    adapter.deposit(compoundDefaultAmount, bob);
+
+    uint256 oldTotalAssets = adapter.totalAssets();
+    uint256 oldTotalSupply = adapter.totalSupply();
+    uint256 oldIouBalance = iouBalance();
+
+    adapter.pause();
+    adapter.unpause();
+
+    // We simply deposit back into the external protocol
+    // TotalSupply and Assets dont change
+    assertApproxEqAbs(oldTotalAssets, adapter.totalAssets(), _delta_, "totalAssets");
+    assertApproxEqAbs(oldTotalSupply, adapter.totalSupply(), _delta_, "totalSupply");
+    assertApproxEqAbs(asset.balanceOf(address(adapter)), 0, _delta_, "asset balance");
+    assertApproxEqAbs(iouBalance(), oldIouBalance, _delta_, "iou balance");
+
+    // Deposit and mint dont revert
+    vm.startPrank(bob);
+    adapter.deposit(compoundDefaultAmount, bob);
+    adapter.mint(compoundDefaultAmount, bob);
+  }
+
+  
+  /*//////////////////////////////////////////////////////////////
+                              HARVEST
+    //////////////////////////////////////////////////////////////*/
+    
+  function test__harvest() public override {
+    _mintFor(compoundDefaultAmount, bob);
+
+    vm.prank(bob);
+    adapter.deposit(compoundDefaultAmount, bob);
 
     // Skip a year
     vm.warp(block.timestamp + 365.25 days);
 
     // uint256 interest = getApy();
-    uint256 expectedFee = adapter.convertToShares((defaultAmount * 5e16) / 1e18);
+    uint256 expectedFee = adapter.convertToShares((compoundDefaultAmount * 5e16) / 1e18);
     uint256 callTime = block.timestamp;
 
     if (address(strategy) != address(0)) {
@@ -139,33 +377,7 @@ contract CompoundV2AdapterTest is AbstractAdapterTest {
 
     adapter.harvest();
 
-    // assertApproxEqAbs(adapter.highWaterMark(), defaultAmount, _delta_, "highWaterMark");
-    assertApproxEqAbs(adapter.totalSupply(), defaultAmount + expectedFee, _delta_, "totalSupply");
-  }
-
-  /*//////////////////////////////////////////////////////////////
-                          ROUNDTRIP TESTS
-    //////////////////////////////////////////////////////////////*/
-
-  function test__RT_deposit_withdraw() public override {
-    _mintFor(defaultAmount, bob);
-
-    vm.startPrank(bob);
-    uint256 shares1 = adapter.deposit(defaultAmount, bob);
-    uint256 shares2 = adapter.withdraw(defaultAmount - 1, bob, bob);
-    vm.stopPrank();
-
-    assertApproxGeAbs(shares2, shares1, _delta_, testId);
-  }
-
-  function test__RT_mint_withdraw() public override {
-    _mintFor(adapter.previewMint(defaultAmount), bob);
-
-    vm.startPrank(bob);
-    uint256 assets = adapter.mint(defaultAmount, bob);
-    uint256 shares = adapter.withdraw(assets - 1, bob, bob);
-    vm.stopPrank();
-
-    assertApproxGeAbs(shares, defaultAmount, _delta_, testId);
+    // assertApproxEqAbs(adapter.highWaterMark(), compoundDefaultAmount, _delta_, "highWaterMark");
+    assertApproxEqAbs(adapter.totalSupply(), compoundDefaultAmount + expectedFee, _delta_, "totalSupply");
   }
 }
