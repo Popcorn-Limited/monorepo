c7c10250d8640a032508b04b7c7606e3b4dd0d4a
ftperez11 <francistp11@gmail.com>
2023-02-15 07:32:17 -0500

    adding adapter contract and interface need to fix totalAssets visibility

---
+++ packages/foundry/src/vault/adapter/masterChef/IMasterChef.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
+
+pragma solidity ^0.8.15;
+
+import { IERC20Upgradeable as IERC20 } from "openzeppelin-contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+interface IMasterChef {
+  struct PoolInfo {
+    IERC20 lpToken; // Address of LP token contract.
+    uint256 allocPoint; // How many allocation points assigned to this pool. SUSHI to distribute per block.
+    uint256 lastRewardBlock; // Last block number that SUSHI distribution occurs.
+    uint256 accSushiPerShare; // Accumulated SUSHI per share, times 1e12. See below.
+  }
+
+  function poolInfo(uint256 pid) external view returns (IMasterChef.PoolInfo memory);
+
+  function totalAllocPoint() external view returns (uint256);
+
+  function deposit(uint256 _pid, uint256 _amount) external;
+
+  function withdraw(uint256 _pid, uint256 _amount) external;
+
+  function enterStaking(uint256 _amount) external;
+
+  function leaveStaking(uint256 _amount) external;
+
+  function emergencyWithdraw(uint256 _pid) external;
+}
+
+interface IRewarder is IERC20 {
+  function onSushiReward(
+    uint256 pid,
+    address user,
+    address recipient,
+    uint256 sushiAmount,
+    uint256 newLpAmount
+  ) external;
+
+  function pendingTokens(
+    uint256 pid,
+    address user,
+    uint256 sushiAmount
+  ) external view returns (IERC20[] memory, uint256[] memory);
+}
---
+++ packages/foundry/src/vault/adapter/masterChef/MasterChefAdapter.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
+
+pragma solidity ^0.8.15;
+
+import { AdapterBase, IERC20, IERC20Metadata, SafeERC20, ERC20, Math, IStrategy, IAdapter } from "../abstracts/AdapterBase.sol";
+import { WithRewards, IWithRewards } from "../abstracts/WithRewards.sol";
+import { IMasterChef, IRewarder } from "./IMasterChef.sol";
+
+//import {IPermissionRegistry} from "../../../interfaces/vault/IPermissionRegistry.sol";
+
+/**
+ * @title   MasterChef Adapter
+ * @notice  ERC4626 wrapper for MasterChef Vaults.
+ *
+ * An ERC4626 compliant Wrapper for https://github.com/sushiswap/sushiswap/blob/archieve/canary/contracts/MasterChefV2.sol.
+ * Allows wrapping MasterChef Vaults.
+ */
+contract MasterChefAdapter is AdapterBase, WithRewards {
+  using SafeERC20 for IERC20;
+  using Math for uint256;
+
+  string internal _name;
+  string internal _symbol;
+
+  IMasterChef public masterChef;
+  IRewarder public rewards;
+
+  uint256 public pid;
+
+  /**
+   * @notice Initialize a new MasterChef Adapter.
+   * @param adapterInitData Encoded data for the base adapter initialization.
+   * @dev `_pid` - The poolId for lpToken.
+   * @dev This function is called by the factory contract when deploying a new vault.
+   */
+  function initialize(
+    bytes memory adapterInitData,
+    address registry,
+    bytes memory masterchefInitData
+  ) external initializer {
+    (address _masterChef, uint256 _pid) = abi.decode(masterchefInitData, (address, uint256));
+
+    masterChef = IMasterChef(_masterChef);
+
+    __AdapterBase_init(adapterInitData);
+
+    pid = _pid;
+
+    // masterChef = IMasterChef(_masterChef);
+
+    // (address _asset, , , address _rewarder, , ) = masterChef.poolInfo(pid);
+
+    IMasterChef.PoolInfo memory info = masterChef.poolInfo(_pid);
+
+    // _rewarder = IRewarder(_rewarder);
+
+    _name = string.concat("Popcorn MasterChef", IERC20Metadata(asset()).name(), " Adapter");
+    _symbol = string.concat("popB-", IERC20Metadata(asset()).symbol());
+
+    IERC20(info.lpToken).approve(address(masterChef), type(uint256).max);
+  }
+
+  function name() public view override(IERC20Metadata, ERC20) returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view override(IERC20Metadata, ERC20) returns (string memory) {
+    return _symbol;
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                            ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+  /// @notice Calculates the total amount of underlying tokens the Vault holds.
+  /// @return The total amount of underlying tokens the Vault holds.
+  //   function totalAssets() public view virtual override returns (uint256) {
+  //     return paused() ? IERC20(asset()).balanceOf(address(this)) : rewards.balanceOf(address(this));
+  //   }
+
+  function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
+    return _convertToShares(assets, Math.Rounding.Up);
+  }
+
+  function previewRedeem(uint256 shares) public view override returns (uint256) {
+    return _convertToAssets(shares, Math.Rounding.Up);
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                          INTERNAL HOOKS LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+  function _protocolDeposit(uint256 amount, uint256) internal virtual override {
+    masterChef.deposit(pid, amount);
+  }
+
+  function _protocolWithdraw(
+    uint256 assets,
+    uint256 shares,
+    uint256 pool
+  ) internal virtual {
+    masterChef.withdraw(pool, convertToUnderlyingShares(assets, shares));
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                      EIP-165 LOGIC
+  //////////////////////////////////////////////////////////////*/
+
+  function supportsInterface(bytes4 interfaceId) public pure override(WithRewards, AdapterBase) returns (bool) {
+    return interfaceId == type(IWithRewards).interfaceId || interfaceId == type(IAdapter).interfaceId;
+  }
+}
