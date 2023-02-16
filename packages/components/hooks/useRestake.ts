1d0c96faa2a8b9bb2d16a38b8166bbe53ac50a3d
RedVeil <leon.niesler@gmail.com>
2023-02-16 12:36:22 +0100

    rewards + staking fixes

--- packages/components/hooks/useRestake.ts
+++ packages/components/hooks/useRestake.ts
@@ -5,7 +5,7 @@ import { useContractWrite, usePrepareContractWrite } from "wagmi";
 const useRestake = (restake: boolean, address: string, chainId: ChainId, wagmiConfig?: ContractWriteArgs) => {
   const { config } = usePrepareContractWrite({
     address,
-    abi: ["function function processExpiredLocks(bool _relock) external"],
+    abi: ["function processExpiredLocks(bool _relock) external"],
     functionName: "processExpiredLocks",
     args: [restake],
     chainId: Number(chainId),
