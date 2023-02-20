7d13ad808c7839aac212a016d3aca58d69560260
RedVeil <leon.niesler@gmail.com>
2023-02-20 12:09:54 +0100

    disclaimer lint issue fixed

--- packages/greenfield-app/pages/disclaimer.tsx
+++ packages/greenfield-app/pages/disclaimer.tsx
@@ -1,64 +1,99 @@
-
 export default function DisclaimerPage(): JSX.Element {
   return (
     <div className="space-y-4">
-      <h2 className="font-bold underline">
-        Introduction
-      </h2>
+      <h2 className="font-bold underline">Introduction</h2>
       <p>
-        Please read this <span className="font-bold">disclaimer</span> carefully before accessing, interacting with, or using popcorndao.finance, consisting of the popcorndao.finance smart contract technology stack and the user interface for the Popcorn DeFi application, as well as any other application developed in the future (together the <span className="font-bold">“Popcorn Software”</span>).
+        Please read this <span className="font-bold">disclaimer</span> carefully before accessing, interacting with, or
+        using popcorndao.finance, consisting of the popcorndao.finance smart contract technology stack and the user
+        interface for the Popcorn DeFi application, as well as any other application developed in the future (together
+        the <span className="font-bold">“Popcorn Software”</span>).
       </p>
       <p>
-        This disclaimer relates to the use of the Popcorn Software. The Popcorn Software enables its users (<span className="font-bold">“User”</span>) to interact with the yield-generating products developed by and operated by third parties (e.g. Curve, Yearn, Uniswap). Any and all functionalities of the Popcorn Software are of purely technical nature and there is no claim towards any private individual or legal entity in this regard.
+        This disclaimer relates to the use of the Popcorn Software. The Popcorn Software enables its users (
+        <span className="font-bold">“User”</span>) to interact with the yield-generating products developed by and
+        operated by third parties (e.g. Curve, Yearn, Uniswap). Any and all functionalities of the Popcorn Software are
+        of purely technical nature and there is no claim towards any private individual or legal entity in this regard.
       </p>
       <h2 className="font-bold underline">
         No Contractual Relationship to popcorndao.finance / Use of Popcorn Software at Own Risk
       </h2>
       <p>
-        The Popcorn Software runs in a fully decentralized and autonomous manner on the underlying blockchains. popcorndao.finance, as well as any other private individual and/or legal entity, is neither involved in, responsible for nor can in any way influence the operation, running or functioning of the Popcorn Software and/or any of the interactions, collaborations or factual relationships between Users and the smart contracts of the Popcorn Software. popcorndao.finance has neither access to nor any other possibility to control and/or influence the corresponding transactions, allocations and/or decisions made by the Users and the involved smart contracts using the Popcorn Software.
+        The Popcorn Software runs in a fully decentralized and autonomous manner on the underlying blockchains.
+        popcorndao.finance, as well as any other private individual and/or legal entity, is neither involved in,
+        responsible for nor can in any way influence the operation, running or functioning of the Popcorn Software
+        and/or any of the interactions, collaborations or factual relationships between Users and the smart contracts of
+        the Popcorn Software. popcorndao.finance has neither access to nor any other possibility to control and/or
+        influence the corresponding transactions, allocations and/or decisions made by the Users and the involved smart
+        contracts using the Popcorn Software.
       </p>
       <p>
-        In particular, popcorndao.finance or any other (third) party has not and will not enter into any legal or factual relationship with any User of the Popcorn Software. Consequently, popcorndao.finance is not liable to any user for damages, including any general, special, incidental, or consequential damages arising out of the use, in connection with the use or inability to use the Popcorn Software (including but not limited to loss of POP, USDC, USDT, DAI, 3CRV, XEN, 3X, BTR, Arrakis USDC/POP LP, loss of data, business interruption, data being rendered inaccurate or losses sustained by a user or third parties.
+        In particular, popcorndao.finance or any other (third) party has not and will not enter into any legal or
+        factual relationship with any User of the Popcorn Software. Consequently, popcorndao.finance is not liable to
+        any user for damages, including any general, special, incidental, or consequential damages arising out of the
+        use, in connection with the use or inability to use the Popcorn Software (including but not limited to loss of
+        POP, USDC, USDT, DAI, 3CRV, XEN, 3X, BTR, Arrakis USDC/POP LP, loss of data, business interruption, data being
+        rendered inaccurate or losses sustained by a user or third parties.
       </p>
       <p>
-        As the Popcorn Software is of experimental nature it may contain bugs, defects, or errors (including any bug, defect, or error relating to or resulting from the display, manipulation, processing, storage, transmission, or use of data) that may materially and adversely affect the use, functionality, or performance of the Popcorn Software or any product or system containing or used in conjunction with the Popcorn Software.
+        As the Popcorn Software is of experimental nature it may contain bugs, defects, or errors (including any bug,
+        defect, or error relating to or resulting from the display, manipulation, processing, storage, transmission, or
+        use of data) that may materially and adversely affect the use, functionality, or performance of the Popcorn
+        Software or any product or system containing or used in conjunction with the Popcorn Software.
       </p>
       <p>
-        The Popcorn Software has been provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement.
+        The Popcorn Software has been provided "as is", without warranty of any kind, express or implied, including but
+        not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement.
       </p>
-      <h2 className="font-bold underline">
-        Assumption of Risks of Cryptographic Token, Currencies, and Systems
-      </h2>
+      <h2 className="font-bold underline">Assumption of Risks of Cryptographic Token, Currencies, and Systems</h2>
       <p>
-        There are also general risks associated with purchasing, owning, and using cryptographic tokens such as POP, USDC, USDT, DAI, 3CRV, XEN, 3X, BTR, and/or Arrakis USDC/POP LP (hereinafter <span className="font-bold">“Tokens”</span>). In order to access and make use of certain features of the Popcorn Software, a User may need Tokens.
-      </p>
-      <p>
-        Tokens are highly volatile due to many factors including but not limited to adoption, speculation, technology, and security risks. The User understands that he may lose the entire amount invested.
-      </p>
-      <p>
-        User is solely responsible for transacting and holding Tokens while accessing and/or interacting with the Popcorn Software.
+        There are also general risks associated with purchasing, owning, and using cryptographic tokens such as POP,
+        USDC, USDT, DAI, 3CRV, XEN, 3X, BTR, and/or Arrakis USDC/POP LP (hereinafter{" "}
+        <span className="font-bold">“Tokens”</span>). In order to access and make use of certain features of the Popcorn
+        Software, a User may need Tokens.
       </p>
       <p>
-        A lack of use or public interest in the creation and development of distributed ecosystems could negatively impact the development of those ecosystems and related applications, (incl. the Popcorn Software), and could therefore also negatively impact the potential utility or value of a certain Tokens.
+        Tokens are highly volatile due to many factors including but not limited to adoption, speculation, technology,
+        and security risks. The User understands that he may lose the entire amount invested.
       </p>
       <p>
-        User is responsible for implementing reasonable measures for securing the wallet, vault or other storage mechanism used to receive and hold Tokens, including any requisite private key(s) or other credentials necessary to access such storage mechanism(s). If User’s private key(s) or other access credentials are lost, User may lose access to its Tokens.
+        User is solely responsible for transacting and holding Tokens while accessing and/or interacting with the
+        Popcorn Software.
       </p>
       <p>
-        There are risks associated with using internet and blockchain-based products, including but not limited to, the risk associated with hardware, software, and internet connections, the risk of malicious software introduction, and the risk that third parties may obtain unauthorized access to information stored within the User’s wallet. The User is responsible for any communication failures, disruptions, errors, distortions, or delays the User may experience when using the Popcorn Software or any blockchain network for transactions, however caused.
-      </p>
-      <h2 className="font-bold underline">
-        Risk of Regulatory Actions and Compliance with Laws
-      </h2>
-      <p>
-        The use of the Popcorn Software is prohibited for persons or entities who reside in, are citizens of, are incorporated in, are located in, or have a registered office in the United States of America, Russian Federation, Myanmar (Burma), Cote D'Ivoire (Ivory Coast), Cuba, Crimea and Sevastopol, Democratic Republic of Congo, Iran, Iraq, Libya, Mali, Nicaragua, Democratic People’s Republic of Korea (North Korea), Somalia, Sudan, Syria, Yemen, Zimbabwe, or are listed, or associated with any person or entity listed, on any sanction list of the governments of the United States, United Kingdom and/or any member state of the European Union.
+        A lack of use or public interest in the creation and development of distributed ecosystems could negatively
+        impact the development of those ecosystems and related applications, (incl. the Popcorn Software), and could
+        therefore also negatively impact the potential utility or value of a certain Tokens.
       </p>
       <p>
-        The Popcorn Software could be impacted by one or more regulatory inquiries or regulatory actions, which could impede or limit User’s ability to access or use the Popcorn Software.
+        User is responsible for implementing reasonable measures for securing the wallet, vault or other storage
+        mechanism used to receive and hold Tokens, including any requisite private key(s) or other credentials necessary
+        to access such storage mechanism(s). If User’s private key(s) or other access credentials are lost, User may
+        lose access to its Tokens.
       </p>
       <p>
-        User is solely responsible for complying with applicable laws when interacting with the Popcorn Software whatsoever.
+        There are risks associated with using internet and blockchain-based products, including but not limited to, the
+        risk associated with hardware, software, and internet connections, the risk of malicious software introduction,
+        and the risk that third parties may obtain unauthorized access to information stored within the User’s wallet.
+        The User is responsible for any communication failures, disruptions, errors, distortions, or delays the User may
+        experience when using the Popcorn Software or any blockchain network for transactions, however caused.
+      </p>
+      <h2 className="font-bold underline">Risk of Regulatory Actions and Compliance with Laws</h2>
+      <p>
+        The use of the Popcorn Software is prohibited for persons or entities who reside in, are citizens of, are
+        incorporated in, are located in, or have a registered office in the United States of America, Russian
+        Federation, Myanmar (Burma), Cote D'Ivoire (Ivory Coast), Cuba, Crimea and Sevastopol, Democratic Republic of
+        Congo, Iran, Iraq, Libya, Mali, Nicaragua, Democratic People’s Republic of Korea (North Korea), Somalia, Sudan,
+        Syria, Yemen, Zimbabwe, or are listed, or associated with any person or entity listed, on any sanction list of
+        the governments of the United States, United Kingdom and/or any member state of the European Union.
+      </p>
+      <p>
+        The Popcorn Software could be impacted by one or more regulatory inquiries or regulatory actions, which could
+        impede or limit User’s ability to access or use the Popcorn Software.
+      </p>
+      <p>
+        User is solely responsible for complying with applicable laws when interacting with the Popcorn Software
+        whatsoever.
       </p>
     </div>
-  )
+  );
 }
