877475424dca0277c1926686eb1b4cb26d90280c
D3Portillo <d3portillo@gmail.com>
2023-03-03 15:36:39 +0100

    enh(_app.tsx): update font - Roboto

--- packages/vci/pages/_app.tsx
+++ packages/vci/pages/_app.tsx
@@ -1,12 +1,53 @@
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
-import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Roboto } from "@next/font/google";
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { goerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { Toaster } from "react-hot-toast";
+
+const { provider, chains } = configureChains([goerli], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "app.pop.network",
+  chains,
+});
+
+const client = createClient({
+  autoConnect: false,
+  provider,
+  connectors,
+});
+
+const nextFont = Roboto({
+  weight: ["400", "700", "900"],
+  subsets: [],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
+    <main className={nextFont.className}>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+            :root [data-rk] {
+              --rk-radii-modal: 1rem;
+            }
+            [data-rk] * {
+              font-family: ${nextFont.style.fontFamily} !important;
+            }
+          `,
+        }}
+      />
+      <Toaster />
+      <WagmiConfig client={client}>
+        <RainbowKitProvider chains={chains} modalSize="compact">
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </main>
   );
 }
