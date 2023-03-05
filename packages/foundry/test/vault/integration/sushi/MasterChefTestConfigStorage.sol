cd4bab62168069e27833c42c5cf137c2bfd18d74
ftperez11 <francistp11@gmail.com>
2023-03-05 16:45:55 -0500

    added reward token to testConfig, all 36 tests pass

--- packages/foundry/test/vault/integration/sushi/MasterChefTestConfigStorage.sol
+++ packages/foundry/test/vault/integration/sushi/MasterChefTestConfigStorage.sol
@@ -4,17 +4,18 @@ import { ITestConfigStorage } from "../abstract/ITestConfigStorage.sol";
 
 struct MasterChefTestConfig {
   uint256 pid;
+  address rewardsToken;
 }
 
 contract MasterChefTestConfigStorage is ITestConfigStorage {
   MasterChefTestConfig[] internal testConfigs;
 
   constructor() {
-    testConfigs.push(MasterChefTestConfig(2));
+    testConfigs.push(MasterChefTestConfig(2, 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82));
   }
 
   function getTestConfig(uint256 i) public view returns (bytes memory) {
-    return abi.encode(testConfigs[i].pid);
+    return abi.encode(testConfigs[i].pid, testConfigs[i].rewardsToken);
   }
 
   function getTestConfigLength() public view returns (uint256) {
