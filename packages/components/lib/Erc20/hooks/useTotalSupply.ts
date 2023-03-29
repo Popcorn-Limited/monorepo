99368c33f8dc19138fa10d73d4392cc9093b2706
D3Portillo <d3portillo@gmail.com>
2023-03-29 15:46:03 +0200

    REFACTOR!: apply better repolling from 65f324146513aac0a3868748a78ddb02a47702fb

--- packages/components/lib/Erc20/hooks/useTotalSupply.ts
+++ packages/components/lib/Erc20/hooks/useTotalSupply.ts
@@ -1,22 +1,23 @@
 import { useContractRead } from "wagmi";
 import { BigNumber } from "ethers/lib/ethers";
-import { formatAndRoundBigNumber } from "@popcorn/utils";
+import { formatAndRoundBigNumber, useConsistentRepolling } from "@popcorn/utils";
 import { BigNumberWithFormatted, Pop } from "../../types";
 
 export const useTotalSupply: Pop.Hook<BigNumberWithFormatted> = ({ chainId, address }) => {
-  return useContractRead({
-    address,
-    chainId: Number(chainId),
-    abi: ["function totalSupply() external view returns (uint256)"],
-    functionName: "totalSupply",
-    scopeKey: `totalSupply:${chainId}:${address}`,
-    enabled: !!address && !!chainId,
-    select: (data) => {
-      return {
-        value: (data as BigNumber) || BigNumber.from(0),
-        formatted: formatAndRoundBigNumber(data as BigNumber, 18),
-      };
-    },
-    watch: true
-  }) as Pop.HookResult<BigNumberWithFormatted>;
+  return useConsistentRepolling(
+    useContractRead({
+      address,
+      chainId: Number(chainId),
+      abi: ["function totalSupply() external view returns (uint256)"],
+      functionName: "totalSupply",
+      scopeKey: `totalSupply:${chainId}:${address}`,
+      enabled: Boolean(address && chainId),
+      select: (data) => {
+        return {
+          value: (data as BigNumber) || BigNumber.from(0),
+          formatted: formatAndRoundBigNumber(data as BigNumber, 18),
+        };
+      },
+    }),
+  ) as Pop.HookResult<BigNumberWithFormatted>;
 };
