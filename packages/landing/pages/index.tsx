60bb850b006d93d9bc0ba8a07455ee47fb520f24
AmirJ <amir1jab@gmail.com>
2023-04-04 14:42:07 +0100

    first attempt with broken scrolling animation

--- packages/landing/pages/index.tsx
+++ packages/landing/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import Link from 'next/link';
 import DesktopMenu from '../components/DesktopMenu';
 import MainActionButton from "../components/Common/MainActionButton";
@@ -9,6 +9,11 @@ import GithubIcon from "@popcorn/components/components/SVGIcons/GithubIcon";
 import DiscordIcon from "@popcorn/components/components/SVGIcons/DiscordIcon";
 import TwitterIcon from "@popcorn/components/components/SVGIcons/TwitterIcon";
 import MediumIcon from "@popcorn/components/components/SVGIcons/MediumIcon";
+import SecondaryActionButton from "@popcorn/app/components/SecondaryActionButton";
+import Slider from "react-slick";
+
+const activeDot = "bg-white";
+let inactiveDot = "bg-white bg-opacity-50";
 
 const IndexPage = () => {
   const router = useRouter();
@@ -19,17 +24,174 @@ const IndexPage = () => {
     }
   }, [router.pathname]);
 
+  // useEffect(() => {
+  //   const scrollContainer = document.querySelector('#scrollContainer');
+  //   scrollContainer.addEventListener('wheel', (event) => {
+  //     event.preventDefault();
+
+  //     console.log(event)
+
+  //     scrollContainer.scrollBy({
+  //       left: event?.scrollLeft + event?.deltaY
+
+  //     });
+  //   });
+  // }, []);
+
+  const AMOUNT_TO_SHIFT = '35.7vw';
+
+  const [locked, setLocked] = useState(Date.now());
+  const myStateRef = useRef(locked);
+  const setMyState = data => {
+    myStateRef.current = data;
+    setLocked(data);
+  };
+
+  const [scrollPos, setScrollPos] = useState(0);
+  const scrollRef = useRef(scrollPos);
+  const _setScrollPos = data => {
+    scrollRef.current = data;
+    setScrollPos(data);
+  };
+
+  const [freeze, setFreeze] = useState(false);
+  const freezeRef = useRef(freeze);
+  const _setFreeze = data => {
+    freezeRef.current = data;
+    setFreeze(data);
+  };
+
+
+
+
+  useEffect(() => {
+    const scrollContainer = document.getElementById('scrollContainer');
+    const prevSection = document.getElementById('prev');
+    const dowell = document.getElementById('do-well');
+    const titleElementForTrigger = document.getElementById('titlex');
+    const nextSection = document.getElementById('xoxop');
+
+
+    window.addEventListener('scroll', (event) => {
+      event.preventDefault();
+      const dateNow = Date.now();
+      // let currentScrollPos = window.scrollY;
+      // const scrollDown = currentScrollPos - scrollRef.current > 0;
+      // _setScrollPos(currentScrollPos);
+
+      // // console.log(dowell.getBoundingClientRect(), currentScrollPos, 'lol');
+      // // console.log(event, 'event')
+      // if (dateNow - myStateRef.current > 100) {
+      if (dateNow - myStateRef.current > 900) {
+        if (dowell.getBoundingClientRect().bottom < 50 && titleElementForTrigger.getBoundingClientRect().top > 300) {
+          setMyState(dateNow);
+          // document.body.style.overflow = 'hidden';
+          titleElementForTrigger.scrollIntoView({ behavior: 'smooth' });
+        }
+
+        if (titleElementForTrigger.getBoundingClientRect().top < -900) {
+          setMyState(dateNow);
+          titleElementForTrigger.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+      // setFreeze(false);
+      // }
+    }, { passive: true })
+
+    // });
+
+
+
+    console.log(titleElementForTrigger.getBoundingClientRect(), 'title')
+    scrollContainer.addEventListener('wheel', (event) => {
+      event.preventDefault();
+      const dateNow = Date.now();
+
+
+      if (dateNow - myStateRef.current > 30) {
+        if (event.deltaY > 0) {
+
+          if (scrollContainer.style.transform === `translateX(-${AMOUNT_TO_SHIFT})`) {
+            console.log('oi')
+            return nextSection.scrollIntoView({ behavior: 'smooth' });
+          } else {
+            setMyState(dateNow);
+            scrollContainer.style.transform = `translateX(-${AMOUNT_TO_SHIFT})`;
+
+          }
+
+        } else if (event.deltaY < 0) {
+
+          if (scrollContainer.style.transform === `translateX(0px)`) {
+            return prevSection.scrollIntoView({ behavior: 'smooth' });
+
+          } else {
+            setMyState(dateNow);
+            scrollContainer.style.transform = `translateX(0px)`;
+            // setTimeout(() => { setMyState(false) }, 2000)
+          }
+        }
+      } else {
+        // setTimeout(() => { setMyState(false) }, 20000)
+        setMyState(dateNow)
+      }
+    });
+  }, [locked]);
+
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const customSlider = useRef(null);
+
+  const gotoSlide = (id) => {
+    setCurrentSlide(id);
+    customSlider.current.slickGoTo(id);
+  };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    easing: "easeInOut",
+    pauseOnHover: false,
+    beforeChange: (oldIndex: number, newIndex: number) => {
+      setCurrentSlide(newIndex);
+    },
+  };
+
+  const tutorialSteps: Array<{ title: string; content: string }> = [
+    {
+      title: "Step 1 - Begin the Minting Process",
+      content:
+        "First connect your wallet. Then select the token you would like to deposit from the dropdown, enter the deposit amount and click ‘Mint’. If you are depositing for the first time, you’ll need to approve the contract.",
+    },
+    {
+      title: "Step 2 – Wait for the batch to process",
+      content: `Your deposits will be held in Butters batch processing queue. Note: To minimise gas fees, deposits are processed approximately every 24 hours. You are able to withdraw your deposits during this phase.`,
+    },
+    {
+      title: `Step 3 – Claim your minted Butter,`,
+      content: `Once the batch has been processed, you will be able to claim the new minted "Butter"`,
+    },
+  ];
+
+
+
+
   return (
     <div className="absolute left-0 flex flex-col">
-      <div className="flex-col w-full h-full smmd:h-screen min-h-[600px] w-screen relative flex">
+      <div className="flex-col w-full h-full smmd:h-screen min-h-[600px] w-screen relative flex smmd:mb-[100vh]">
         <DesktopMenu />
         <div className="flex flex-row justify-end absolute right-0 top-96 smmd:top-24">
           <img alt="" className="smmd:w-[50vw] w-[60vw] smmd:max-w-[700px] smmd:max-h-[80vh]" src="/images/icons/greenPopLogo.svg" />
         </div>
-        <section className="relative px-6 smmd:px-8 smmd:mb-0 smmd:h-[90vh] justify-between items-start w-full">
+        <section id="prev" className="relative px-6 smmd:px-8 smmd:mb-0 smmd:h-[90vh] justify-between items-start w-full overflow-scroll">
           <div className="flex flex-col items-start smmd:h-full smmd:w-full pb-16 smmd:pb-0 mx-auto min-w-480 lg:flex-row">
             <div className=" text-center z-20 smmd:text-left smmd:mx-0 flex flex-col justify-around h-full w-full">
-              <h1 className="mb-6 leading-[60px] smmd:leading-[72px] font-normal min-h-[80px] max-w-[450px] lglaptop:max-w-[900px] smmd:max-w-[700px] text-left text-5xl smmd:text-6xl lglaptop:text-[96px] lglaptop:leading-[90px] smmd:w-1/2 mt-24 smmd:mt-0 ">
+              <h1 id="do-well" className="mb-6 leading-[60px] smmd:leading-[72px] font-normal min-h-[80px] max-w-[450px] lglaptop:max-w-[900px] smmd:max-w-[700px] text-left text-5xl smmd:text-6xl lglaptop:text-[96px] lglaptop:leading-[90px] smmd:w-1/2 mt-24 smmd:mt-0 ">
                 Do well and do good together with Popcorn’s innovative DeFi solutions
               </h1>
               <div className="w-fit flex mb-48 smmd:mb-0">
@@ -67,8 +229,76 @@ const IndexPage = () => {
         </div>
       </div>
 
+      <section className="px-8">
+        <div className="mt-[200px] mb-18 flex flex-col relative">
+          <p id="titlex" className="text-6xl">Earn high returns on your crypto while funding public goods</p>
+          <div id="scrollContainer" className="flex flex-row h-[650px] overflow-x-auto transition-transform">
+            <div className="bg-black w-[467px] h-full mr-12 shrink-0">
 
-      <section className="w-full pt-14">
+            </div>
+
+            <div className="flex flex-col w-screen">
+              <div className="flex flex-col w-[338px] mb-[100px]">
+                <p className="text-lg">It's now easy to deposit your crypto, optimise your yield, and create positive global impact at the same time.</p>
+                <div className="flex flex-row border-x-0 border-y-customLightGray border-b-2">
+                  <p className="px-1 py-2 w-full mt-2 leading-7">Create Account</p>
+                  <SecondaryActionButton label="" />
+                </div>
+              </div>
+
+              <div className="w-full h-full flex-row flex gap-x-10 w-screen">
+                <div className="w-full h-full bg-black">
+
+                </div>
+
+                <div className="w-full h-full bg-black">
+
+                </div>
+
+                <div className="w-full h-full bg-black">
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section >
+
+
+      <div className="relative">
+        <Slider {...settings} ref={(slider) => (customSlider.current = slider)}>
+          {tutorialSteps.map((step, index) => (
+            <div className="" key={index}>
+              <div className="bg-customPurple rounded-lg h-110 p-8 flex flex-col justify-between text-white">
+                <h6>How It Works</h6>
+
+                <div className="py-24">
+                  <h3 className="font-medium text-2xl">{step.title}</h3>
+                  <p>{step.content}</p>
+                </div>
+
+                <div className="flex justify-end pt-6 gap-5 md:gap-0 md:space-x-5">
+                  {tutorialSteps.map((steps, index) => (
+                    <div
+                      className={`${currentSlide == index ? activeDot : inactiveDot} rounded-full h-3 w-3 transition-all`}
+                      onClick={() => gotoSlide(index)}
+                      key={index}
+                    ></div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+
+
+
+
+
+      <section id="xoxop" className="w-full pt-14">
         <div className="flex flex-col md:flex-row justify-between w-11/12 pb-12 mx-auto border-b border-gray-500">
           <Link href="/" passHref>
             <img src="/images/icons/popLogo.svg" alt="Logo" className="h-10 hidden md:block"></img>
@@ -156,7 +386,7 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
-    </div>
+    </div >
 
   );
 };
