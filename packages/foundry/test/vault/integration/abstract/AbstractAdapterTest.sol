32f417e9795bd695b2bade50dd5d96569c9ba16f
RedVeil <leon.niesler@gmail.com>
2023-04-10 11:11:38 +0200

    using hardhat addresses for bob and alice

--- packages/foundry/test/vault/integration/abstract/AbstractAdapterTest.sol
+++ packages/foundry/test/vault/integration/abstract/AbstractAdapterTest.sol
@@ -25,8 +25,8 @@ contract AbstractAdapterTest is PropertyTest {
   bytes32 constant PERMIT_TYPEHASH =
     keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
-  address bob = address(1);
-  address alice = address(2);
+  address bob = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+  address alice = address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
   address feeRecipient = address(0x4444);
 
   uint256 defaultAmount;
