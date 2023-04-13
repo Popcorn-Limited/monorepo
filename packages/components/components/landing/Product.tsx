7d701c973991412953d3dcc55180303960e3c26d
D3Portillo <d3portillo@gmail.com>
2023-04-13 16:40:51 +0200

    feat: PRODUCTS section for dapp landing

--- packages/components/components/landing/Product.tsx
+++ packages/components/components/landing/Product.tsx
@@ -1,8 +1,7 @@
-import MainActionButton from "@popcorn/app/components/MainActionButton";
-import StatusWithLabel, { StatusWithLabelProps } from "@popcorn/app/components/Common/StatusWithLabel";
-import useNetworkName from "@popcorn/app/hooks/useNetworkName";
-import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import StatusWithLabel, { StatusWithLabelProps } from "@popcorn/app/components/Common/StatusWithLabel";
+import Link from "next/link";
 
 export interface ProductProps {
   title: string;
@@ -13,39 +12,34 @@ export interface ProductProps {
   badge?: string;
 }
 
-export default function Product({ title, description, stats, route, customContent, badge }: ProductProps): JSX.Element {
+export default function Product({ title, description, stats, badge, customContent, route }: ProductProps): JSX.Element {
   return (
-    <div className="border-b border-customLightGray  border-opacity-40 grid grid-cols-12 items-center gap-6 md:gap-8 py-7">
-      <div className="col-span-12 md:col-span-4 order-1">
+    <Link
+      target="_blank"
+      href={route}
+      className="group border rounded lg:w-screen max-w-[25rem] lg:max-w-[21.5rem] flex relative flex-col bg-[#FAF9F4] border-warmGray border-opacity-75 items-center gap-6 md:gap-8 p-7"
+    >
+      {badge && (
+        <img
+          src={badge}
+          alt={`badge-${title}`}
+          className="hidden w-16 md:inline-block absolute top-0 right-0 translate-x-1/2 -translate-y-1/2"
+        />
+      )}
+      <div className="col-span-12 md:col-span-4">
         <div className="relative flex flex-row">
           <p className="text-black text-4xl leading-9 md:leading-10 mb-2">{title}</p>
-          {badge && <img src={badge} alt={`badge-${title}`} className="hidden md:inline-block ml-8 -mt-28" />}
         </div>
         <p className=" text-primaryDark">{description}</p>
       </div>
 
-      <div className="col-span-12 md:col-span-3 md:order-2 hidden md:block">{customContent}</div>
-      {customContent && (
-        <div className="col-span-12 md:col-span-3 order-3 md:hidden">{customContent && customContent}</div>
-      )}
-
-      <div className="col-span-12 md:col-span-3 grid grid-cols-12 order-2 md:order-3">
-        <div className="col-span-6">
-          <StatusWithLabel content={stats[0].content} label={stats[0].label} infoIconProps={stats[0].infoIconProps} />
-        </div>
-
-        <div className="col-span-6">
-          {stats.length === 2 && (
-            <StatusWithLabel content={stats[1].content} label={stats[1].label} infoIconProps={stats[1].infoIconProps} />
-          )}
-        </div>
+      <div className="flex flex-grow items-center justify-end w-full my-8">{customContent}</div>
+      <div className="flex justify-between w-full">
+        <StatusWithLabel content={stats[0].content} label={stats[0].label} infoIconProps={stats[0].infoIconProps} />
+        {stats.length === 2 && (
+          <StatusWithLabel content={stats[1].content} label={stats[1].label} infoIconProps={stats[1].infoIconProps} />
+        )}
       </div>
-
-      <div className="col-span-12 md:col-span-2 order-4">
-        <Link href={`/${route}`} passHref>
-          <MainActionButton label="View" />
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 }
