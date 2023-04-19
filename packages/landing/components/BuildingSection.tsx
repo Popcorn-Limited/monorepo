514ec89a183729b7525dacd59c845c18a1d50cb9
RedVeil <leon.niesler@gmail.com>
2023-04-19 18:41:13 +0200

    adjusted gitcoin section

--- packages/landing/components/BuildingSection.tsx
+++ packages/landing/components/BuildingSection.tsx
@@ -25,7 +25,7 @@ export default function BuildingSection() {
 
   return (
     <>
-      <section className="flex w-screen h-screen min-h-[800px] flex-col relative justify-start -z-10">
+      <section className="flex w-screen h-screen max-h-[800px] flex-col relative justify-start -z-10">
         <div style={{ background: 'linear-gradient(270deg, rgba(0, 0, 0, 5e-05) 50%, rgba(0, 0, 0, 0.25) 100%), linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0.09%, rgba(0, 0, 0, 5e-05) 100%), linear-gradient(360deg, rgba(0, 0, 0, 0.7) 0.04%, rgba(0, 0, 0, 0.0001) 42.85%), url(/images/background-rectangle.png)', 'WebkitBackgroundSize': 'cover' }} className="relative top-0 left-0 flex flex-col justify-between w-screen grow pb-0 bg-cover">
 
           <div className="w-full grow flex-col flex justify-start items-start xl:max-w-[1800px] xl:mx-auto">
@@ -37,23 +37,16 @@ export default function BuildingSection() {
           </div>
 
 
-          <div className="w-full grow flex-col flex justify-start items-end px-8 smmd:px-0 xl:max-w-[1800px] xl:mx-auto">
+          <div className="w-full grow flex flex-col justify-end items-end px-8 mb-10 smmd:px-0 xl:max-w-[1800px] xl:mx-auto">
             <div className={`${isVisible ? 'smmd:-translate-x-8' : 'smmd:translate-x-full'} shrink transition-transform smmd:w-[40vw] w-full delay-[200ms] duration-[800ms] `} >
-              <p className="text-white text-right smmd:text-left">
+              <p className="text-white text-right text-lg">
                 At Popcorn Network, we&#39;re not just about financial gains, we&#39;re also about making a difference. That&#39;s why we partner and donate a percentage of our profits to Gitcoin, a platform that supports social impact organizations across the globe. By investing in our DeFi suite, you&#39;re not just earning rewards, you&#39;re also investing in change
               </p>
 
-              <div className="flex-col flex mt-20">
-                <p className="text-white text-xl mb-4">Organizations Supported</p>
-                <div className="flex-row items-center w-fit px-2 gap-x-4 bg-white smmd:flex hidden">
+              <div className="flex flex-col mt-20 justify-end items-end">
+                <p className="text-white text-sm mb-4 uppercase">Organizations Supported</p>
+                <div className="flex-row items-center w-fit px-2 gap-x-4 bg-white">
                   <img src="/images/icons/GITCOIN.svg" className='h-12' />
-                </div>
-                <div className="col-span-12 block smmd:col-span-8 smmd:col-start-4 px-6 smmd:px-8 pb-6 smmd:hidden">
-                  <SliderContainer slidesToShow={2} settingsOverride={{ arrows: false }}>
-                    <img src="/images/icons/immunifi-logo.png" className='h-6 w-fit px-5' />
-                    <img src="/images/icons/blocksec-logo.png" className='h-6 w-fit px-5' />
-                    <img src="/images/icons/immunifi-logo.png" className='h-6 w-fit px-5' />
-                  </SliderContainer>
                 </div>
               </div>
             </div>
@@ -61,11 +54,10 @@ export default function BuildingSection() {
 
         </div>
       </section >
-      <section className="px-8 flex w-screen h-full py-10 bg-[#FFE650] rounded-t-xl z-10 -mt-2">
-        <section className="px-8 flex w-screen h-full py-10 bg-[#FFE650] rounded-t-xl z-10 -mt-2 xl:max-w-[1800px] xl:mx-auto">
+      <section className="flex w-screen h-full py-10 bg-[#FFE650] rounded-t-xl z-10 -mt-2">
+        <section className="flex w-screen h-full py-10 bg-[#FFE650] rounded-t-xl z-10 -mt-2 xl:max-w-[1800px] xl:mx-auto">
           <div className="flex-col flex w-full gap-y-4">
-
-            <div className="flex flex-col md:flex-row w-full md:h-[50%]">
+            <div className="flex flex-col md:flex-row w-full md:h-[50%] px-8">
               <div className="flex flex-col md:w-[40%]">
                 <p className="text-black text-3xl"><b>Governed by</b></p>
                 <p className="text-black text-3xl -mt-2">PopcornDAO</p>
@@ -79,21 +71,21 @@ export default function BuildingSection() {
               </div>
             </div>
 
-            <div className="flex flex-row w-full md:h-[50%] md:gap-x-3 gap-x-[1.4vw] relative -ml-8 md:ml-0 mt-10 md:mt-0">
+            <div className="flex flex-row w-full md:h-[50%] md:gap-x-3 gap-x-[1.4vw] relative md:ml-0 mt-10 md:mt-0">
               <img src="/images/face4.svg" className="w-[24vw] md:w-[19vw]" />
               <img src="/images/face5.svg" className="w-[24vw] md:w-[19vw]" />
               <img src="/images/face6.svg" className="w-[24vw] md:w-[19vw]" />
               <img src="/images/face7.svg" className="w-[24vw] md:w-[19vw]" />
               <div className="h-full w-[19vw] hidden md:flex flex-col justify-end items-end">
-                <img src="/images/icons/popLogo.svg" className="w-12 h-12" />
+                <img src="/images/icons/popLogo.svg" className="w-12 h-12 mr-8 xl:mr-20" />
               </div>
             </div>
-            <div className="w-full md:hidden flex flex-row gap-x-3 justify-start -ml-8">
+            <div className="w-full md:hidden flex flex-row gap-x-3 justify-start ">
               <img src="/images/face1.svg" className="h-auto w-[24vw]" />
               <img src="/images/face2.svg" className="h-auto w-[24vw]" />
               <img src="/images/face3.svg" className="h-auto w-[24vw]" />
-              <div className="h-full w-[24vw] md:hidden flex flex-col justify-end items-end">
-                <img src="/images/icons/popLogo.svg" className="w-12 h-12 ml-4" />
+              <div className="h-full w-[24vw] md:hidden flex flex-col justify-end items-center">
+                <img src="/images/icons/popLogo.svg" className="w-12 h-12" />
               </div>
             </div>
           </div>
