65414e39430eb13f7668dfd6c6e38998c2252d8f
RedVeil <leon.niesler@gmail.com>
2023-04-20 13:07:22 +0200

    fixed TokenIcon

--- packages/greenfield-app/components/TokenIcon.tsx
+++ packages/greenfield-app/components/TokenIcon.tsx
@@ -15,21 +15,22 @@ export default function TokenIcon({
   chainId,
 }: TokenIconProps): JSX.Element {
   const metadata = useContractMetadata({ address, chainId });
-  if (metadata?.icons?.length > 1) {
+
+  if (metadata?.data?.icons?.length > 1) {
     return (
       <div className="flex flex-row flex-shrink-0 flex-grow-0">
-        <img src={metadata?.icons[0]} alt="token icon" className={imageSize ? imageSize : "w-6 md:w-10 h-6 md:h-10"} />
+        <img src={metadata?.data?.icons[0]} alt="token icon" className={imageSize ? imageSize : "w-6 md:w-10 h-6 md:h-10"} />
         <img
-          src={metadata?.icons[1]}
+          src={metadata?.data?.icons[1]}
           alt="token icon"
           className={`${imageSize ? imageSize : "w-6 md:w-10 h-6 md:h-10"} -ml-3`}
         />
       </div>
     );
   }
-  if (metadata?.icons?.length === 1) {
+  if (metadata?.data?.icons?.length === 1) {
     return (
-      <img src={metadata?.icons[0]} alt="token icon" className={imageSize ? imageSize : "w-6 md:w-10 h-6 md:h-10"} />
+      <img src={metadata?.data?.icons[0]} alt="token icon" className={imageSize ? imageSize : "w-6 md:w-10 h-6 md:h-10"} />
     );
   }
   // Per default show POP icon
